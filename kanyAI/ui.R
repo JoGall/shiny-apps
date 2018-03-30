edb97ae21a8e187c3ba4ef53a68c975b37c4d983
jogall <joedgallagher@gmail.com>
2018-03-30 22:24:02 +0100

    Added kan kanyAI app

--- kanyAI/ui.R
+++ kanyAI/ui.R
@@ -5,21 +5,23 @@ shinyUI(fluidPage(
   titlePanel("kanyAI"),
   sidebarLayout(
     sidebarPanel(
-      helpText("A long short-term memory (LSTM)-based recurrent neural network (RNN) trained on Kanye West lyrics using ",
-               a("Keras for R.", href = "https://keras.rstudio.com/", target="_blank"),
-               "",
-               "Higher temperatures generate predictions with more 'creativity'.
-               Viewer discretion is advised."),
+      helpText("A long short-term memory (LSTM) recurrent neural network (RNN) trained on Kanye West lyrics using",
+               a("Keras and TensorFlow.", href = "https://keras.rstudio.com/", target="_blank")),
+      helpText("Higher temperatures generate more 'creative' predictions."),
+      helpText("Parental advisory: explicit lyrics."),
       hr(),
       sliderInput("nchar",
                   label = "Output length (characters)",
                   min = 100, max = 1000, value = 500),
       sliderInput("temp",
                   label = "Temperature",
-                  min = 0.01, max = 1.5, value = 0.2),
+                  min = 0.01, max = 1, value = 0.18),
       actionButton("go", "Go"),
       hr(),
-      helpText("Scripts available here.")
+      helpText("By ",
+               a("Joe Gallagher", href = "https://jogall.github.io/", target="_blank"),
+               "| Code available ",
+               a("here.", href = "https://github.com/JoGall/shiny-apps/tree/master/kanyAI", target="_blank"))
     ),
     mainPanel(
       fluidRow(
